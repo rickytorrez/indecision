998311c871251794faf9b17904a039c42b529d7c
rickytorrez <torrez.ricky@ymail.com>
2020-07-15 18:47:17 -0400

    defaultProps

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,30 +1,62 @@
-console.log('visible');
+class Visibility extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleToggle = this.handleToggle.bind(this);
+    this.state = {
+      visible: false,
+    };
+  }
 
-const app = {
-  title: 'Visibility Toggle',
-  visible: false,
-  message: 'These are some random facts because we speak the truth',
-};
+  handleToggle() {
+    this.setState((prevState) => {
+      return {
+        visible: !prevState.visible,
+      };
+    });
+  }
 
-const onShowDetails = () => {
-  console.log(app.visible);
-  app.visible = !app.visible;
-  render();
-};
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleToggle}>
+          {this.state.visible ? 'Hide deets' : 'Show deets'}
+        </button>
+        {this.state.visible && <p>Spitting the truth</p>}
+      </div>
+    );
+  }
+}
 
-const appRoot = document.getElementById('app');
+ReactDOM.render(<Visibility />, document.getElementById('app'));
 
-const render = () => {
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      <button onClick={onShowDetails}>
-        {app.visible ? 'Hide Deets' : 'Show deets'}
-      </button>
-      {app.visible ? <p>{app.message}</p> : null}
-    </div>
-  );
-  ReactDOM.render(template, appRoot);
-};
+// console.log('visible');
 
-render();
+// const app = {
+//   title: 'Visibility Toggle',
+//   visible: false,
+//   message: 'These are some random facts because we speak the truth',
+// };
+
+// const onShowDetails = () => {
+//   console.log(app.visible);
+//   app.visible = !app.visible;
+//   render();
+// };
+
+// const appRoot = document.getElementById('app');
+
+// const render = () => {
+//   const template = (
+//     <div>
+//       <h1>{app.title}</h1>
+//       <button onClick={onShowDetails}>
+//         {app.visible ? 'Hide Deets' : 'Show deets'}
+//       </button>
+//       {app.visible ? <p>{app.message}</p> : null}
+//     </div>
+//   );
+//   ReactDOM.render(template, appRoot);
+// };
+
+// render();
